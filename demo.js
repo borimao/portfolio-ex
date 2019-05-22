64a9df6003303e20b665dabd8e5e9c8136e6024b
新堀真雄 <niiborimasao@Pro.local>
2019-05-22 19:35:36 +0900

    レスポンシブ、safari 対応

--- demo.js
+++ demo.js
@@ -4,6 +4,9 @@ var scroll = 100;
 var flag = true;
 var st_flag = true;
 
+var agent
+var chrome
+
 function sr(num){
     if(now_num != num){
         var cover = document.querySelector('.cover');
@@ -18,41 +21,62 @@ function sr(num){
 }
 
 window.onload = ()=>{
-        const myMain = document.querySelector('.center');
-        const rect = myMain.getBoundingClientRect();
-        const scrollTop = window.pageXOffset || document.documentElement.scrollTop;
-        const Top = rect.top + scrollTop;
-        console.log('pokotin')
-        window.scrollTo({
-            top: Top
-        });
-}
-
-
-window.onscroll = () => {
-    scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    
-    if(scroll > 100 && flag == true && now_num < 7){
-        console.log(scroll)
-        flag = false;
-        sr(now_num + 1);
-        window.setTimeout(() => {
-            flag = true;
-        },1500)
-    }else if (scroll < 100 && flag == true && now_num > 1){
-        console.log(scroll)
-        flag = false;
-        sr(now_num - 1);
-        window.setTimeout(() => {
-            flag = true;
-        },1500)
-    }
-
+    agent = window.navigator.userAgent.toLowerCase();
+    chrome = (agent.indexOf('chrome') !== -1) && (agent.indexOf('edge') === -1)  && (agent.indexOf('opr') === -1);
     const myMain = document.querySelector('.center');
     const rect = myMain.getBoundingClientRect();
     const scrollTop = window.pageXOffset || document.documentElement.scrollTop;
     const Top = rect.top + scrollTop;
+    console.log('pokotin')
     window.scrollTo({
         top: Top
     });
+    if(chrome == false){
+        var main = document.querySelector(".main");
+        main.classList.add('m_scroll');
+        var b_list = document.querySelector(".button_list");
+        b_list.classList.add('delete');
+    }
+}
+
+
+window.onscroll = () => {
+    if(chrome == true){
+        scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    
+        if(scroll > 100 && flag == true && now_num < 7){
+            console.log(scroll)
+            flag = false;
+            sr(now_num + 1);
+            window.setTimeout(() => {
+                flag = true;
+            },1500)
+        }else if (scroll < 100 && flag == true && now_num > 1){
+            console.log(scroll)
+            flag = false;
+            sr(now_num - 1);
+            window.setTimeout(() => {
+                flag = true;
+            },1500)
+        }
+
+        const myMain = document.querySelector('.center');
+        const rect = myMain.getBoundingClientRect();
+        const scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+        const Top = rect.top + scrollTop;
+        window.scrollTo({
+            top: Top
+        });
+
+    }
+}
+
+
+function ViwWork(){
+    const w_area = document.querySelector('.w_back');
+    w_area.classList.add('viw_work');
+}
+function CloseWork(){
+    const w_area = document.querySelector('.w_back');
+    w_area.classList.remove('viw_work');
 }
